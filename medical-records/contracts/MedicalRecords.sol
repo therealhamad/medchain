93e7e1ca599448bd3cb30bef5fc0be99229a537a
Hamad Hussain <136684224+therealhamad@users.noreply.github.com>
2024-07-23 22:09:17 +0530

    Update MedicalRecords.sol

--- medical-records/contracts/MedicalRecords.sol
+++ medical-records/contracts/MedicalRecords.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-pragma solidity ^0.8.19;
-
 contract MedicalRecords {
     struct Record {
         string data; // The medical data associated with the records
